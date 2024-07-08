0a5de8ec8ec274007db863bd208d867a4b502f4f
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-07-08 15:25:11 +0900

    spread rest

--- 03_js_basic/060_spread_rest/start/main.js
+++ 03_js_basic/060_spread_rest/start/main.js
@@ -1,22 +1,24 @@
-const nums = [3, 1, 4, 1, 5, 10, 2, 6];
+// const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
-console.log(result);
+// const result = Math.max(...nums);
+// console.log(result);
 
 
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
-let newArr = [...arr1];
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+// let newArr = [...arr1, ...arr2];
+// let newArr1 = arr1
+// newArr1.push(4);
+// console.log(newArr1);
 
-console.log(newArr2);
+// const obj = {
+//   name: "Tom",
+//   age: 22,
+// };
+// const newObj = { ...obj };
+// newObj.name = "Byakko";
 
-const obj = {
-  name: "Tom",
-  age: 22,
-};
-const newObj = { ...obj };
-
-console.log(newObj);
+// console.log(newObj, obj);
 
 const restA = (...argA) => console.log(argA);
 restA(1, 3, 4)
