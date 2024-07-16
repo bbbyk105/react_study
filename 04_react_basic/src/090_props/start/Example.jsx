b3d13c40ad496cee1436de127bb97a945ef552ce
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-07-16 19:59:05 +0900

    式と文とprops

--- 04_react_basic/src/090_props/start/Example.jsx
+++ 04_react_basic/src/090_props/start/Example.jsx
@@ -1,5 +1,10 @@
 import Child from "./components/Child";
 
-const Example = () => <Child />;
+const Example = () =>
+    <>
+        <Child />
+        <Child color="red" />
+    </>
+
 
 export default Example;
