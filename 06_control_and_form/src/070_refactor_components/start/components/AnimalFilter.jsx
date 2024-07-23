715957aef16eb3e3a6bb68a1ff9dd407877b653d
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-07-23 17:00:43 +0900

    componentno

---
+++ 06_control_and_form/src/070_refactor_components/start/components/AnimalFilter.jsx
@@ -0,0 +1,13 @@
+const animalFilter = ({filterState}) =>{
+  const [filterVal, setFilterVal] = filterState;
+    return (
+        <input
+        type="text"
+        value={filterVal}
+        onChange={(e) => 
+        setFilterVal(e.target.value)}
+      />
+    );
+};
+
+export default animalFilter;
