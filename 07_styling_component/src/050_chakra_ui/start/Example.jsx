3fa66b386f65e2ac5e3599fec5b6e55a5d29b370
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-07-29 15:07:29 +0900

    mordalの作り方

--- 07_styling_component/src/050_chakra_ui/start/Example.jsx
+++ 07_styling_component/src/050_chakra_ui/start/Example.jsx
@@ -1,10 +1,14 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
 import Todo from "./components/Todo"
 
 const Example = () => {
   return (
     <>
-      <h2>Reminder</h2>
-      <Todo />
+      <ChakraProvider>
+        <Todo />
+      </ChakraProvider>
+
     </>
   );
 };
--- 07_styling_component/src/050_chakra_ui/start/components/List.jsx
+++ 07_styling_component/src/050_chakra_ui/start/components/List.jsx
@@ -1,18 +1,22 @@
-const List = ({todos, deleteTodo}) => {
+import { VStack } from "@chakra-ui/react";
+
+const List = ({ todos, deleteTodo }) => {
     const complete = (id) => {
         deleteTodo(id)
     }
     return (
-        <div>
-            {todos.map(todo => {
-                return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
-                )
-            })}
-        </div>
+        <VStack>
+            <div>
+                {todos.map(todo => {
+                    return (
+                        <div key={todo.id}>
+                            <button onClick={() => complete(todo.id)}>完了</button>
+                            <span>{todo.content}</span>
+                        </div>
+                    )
+                })}
+            </div>
+        </VStack>
     );
 }
 
