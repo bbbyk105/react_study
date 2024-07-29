3a24d0ebe13b72af9acade78d661e103f13ddab5
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-07-29 16:16:45 +0900

    useRefでの動画の再生、停止

--- 08_other_function/src/030_useRef/start/Example.jsx
+++ 08_other_function/src/030_useRef/start/Example.jsx
@@ -9,18 +9,42 @@ const Case1 = () => {
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" ref ={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick ={()=> inputRef.current.focus()}>
+      <input type="text" ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => inputRef.current.focus()}>
         インプット要素をフォーカスする
       </button >
     </div>
   );
 };
+const Case2 = () => {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef();
+
+  return (
+    <div>
+      <video style={{ maxWidth: "100%" }} ref={videoRef}>
+        <source src="./sample.mp4"></source>
+      </video>
+      <button onClick={() => {
+        if (playing) {
+          videoRef.current.pause();
+        } else {
+          videoRef.current.play();
+        }
+
+        setPlaying(prev => !prev);
+      }} >
+        {playing ? "stop" : "play"}
+      </button>
+    </div>
+  )
+}
 
 const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2 />
     </>
   );
 };
