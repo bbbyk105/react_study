973c125b9ba06154729ac0da175f25dc68404a8f
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-08-01 14:33:00 +0900

    謎のバグ

--- 11_hooks_p1/src/040_useContext_with_state/start/Example.jsx
+++ 11_hooks_p1/src/040_useContext_with_state/start/Example.jsx
@@ -1,14 +1,18 @@
-import { createContext } from "react";
-import Child from "./components/Child";
-import OtherChild from "./components/OtherChild";
-export const MyContext = createContext("hello");
+// POINT useContextとstate
+import { createContext, useState } from "react";
+import Child from "./component/Child";
+import OtherChild from "./component/OtherChild";
+
+export const MyContext = createContext();
 
 const Example = () => {
+  const stateAndSetter = useState(0);
+  // stateAndSetter = [ state, setState ]
   return (
-    <>
+    <MyContext.Provider value={stateAndSetter}>
       <Child />
       <OtherChild />
-    </>
+    </MyContext.Provider>
   );
 };
 
