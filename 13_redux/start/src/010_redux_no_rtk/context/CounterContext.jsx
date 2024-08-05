b3668a8a0b8ee09b260224e62483fd69bfc57804
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-08-05 17:05:00 +0900

    reduxの触り

--- 13_redux/start/src/010_redux_no_rtk/context/CounterContext.jsx
+++ 13_redux/start/src/010_redux_no_rtk/context/CounterContext.jsx
@@ -3,17 +3,20 @@ import { createContext, useContext, useReducer } from "react";
 const CounterContext = createContext();
 const CounterDispatchContext = createContext();
 
+
+const reducer = (prev, { type, step }) => {
+  switch (type) {
+    case "+":
+      return prev + step;
+    case "-":
+      return prev - step;
+    default:
+      return state;
+  }
+};
+
 const CounterProvider = ({ children }) => {
-    const [state, dispatch] = useReducer((prev, { type, step }) => {
-        switch (type) {
-          case "+":
-            return prev + step;
-          case "-":
-            return prev - step;
-          default:
-            throw new Error('不明なactionです。')
-        }
-      }, 0);
+    const [state, dispatch] = useReducer(reducer, 0);
     return (
         <CounterContext.Provider value={state}>
             <CounterDispatchContext.Provider value={dispatch}>
