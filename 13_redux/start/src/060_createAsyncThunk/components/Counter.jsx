b6884583bc581d4a6166852b588e3aaab60378be
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-08-07 18:48:45 +0900

    redux toolkit

--- 13_redux/start/src/060_createAsyncThunk/components/Counter.jsx
+++ 13_redux/start/src/060_createAsyncThunk/components/Counter.jsx
@@ -2,14 +2,17 @@ import { add, minus,addAsync } from "../store/modules/counter"
 
 import CounterResult from "./CounterResult"
 import CounterButton from "./CounterButton"
+import { useSelector } from "react-redux"
 
 const Counter = () => {
+    const status = useSelector(state => state.counter.status);
     return (
         <>
             <CounterResult />
             <CounterButton step={2} calcType="+" actionCreator={add}/>
             <CounterButton step={2} calcType="-" actionCreator={minus}/>
             <CounterButton step={2} calcType="非同期(+)" actionCreator={addAsync}/>
+            <h3></h3>
         </>
     )
 }
