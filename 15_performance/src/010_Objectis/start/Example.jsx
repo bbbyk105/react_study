d89126ff566b579b034946d0cc4a00f67ad56fe0
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-08-07 20:18:32 +0900

    再レンダリング

--- 15_performance/src/010_Objectis/start/Example.jsx
+++ 15_performance/src/010_Objectis/start/Example.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
+
 const Example = () => {
+  console.log("render")
+
+  const [countA, setCountA] = useState({
+    val: 0
+  });
+
+  const obj1 = { val: 0 };
+  const obj3 = { val: 0 };
+  const obj2 = {...obj1};
+  const isSame = Object.is(obj1, obj2);
+  console.log(isSame);
+
   return (
-    <>
-    <h3>再レンダリングが発生する条件</h3>
-    <p>stateの値が変更された時</p>
-    <p>Object.isによって変更は検知される</p>
-    </>
+    <div className="parent">
+      <div>
+        <h3>再レンダリング？</h3>
+        <button
+          onClick={() => {
+            setCountA(prev => {
+              const newState = {...prev};
+              // prev.val = 1;
+              return newState;
+            });
+          }}
+        >
+
+          ボタンA
+        </button>
+      </div>
+      <div>
+        <p>ボタンクリック回数: {countA.val}</p>
+      </div>
+    </div>
   );
 };
 
