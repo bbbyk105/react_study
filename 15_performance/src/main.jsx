a0def8cfbbe923ce12a45a62fd426b763b23c4de
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-08-08 14:59:36 +0900

    子コンポーネントの再レンダリングの確認

--- 15_performance/src/main.jsx
+++ 15_performance/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import lectures from "./lectures.js";
+import { createRoot } from "react-dom/client";
+
 
 const root = createRoot(document.getElementById
   ("root"))
@@ -10,3 +12,4 @@ const root = createRoot(document.getElementById
 root.render(
     <App lectures={lectures} />
 );
+
