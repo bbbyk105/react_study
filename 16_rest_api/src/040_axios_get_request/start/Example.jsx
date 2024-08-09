9b08c9d14aa675707a894bdc84a6368f1db749db
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-08-09 14:46:12 +0900

    dataの取得と表示

--- 16_rest_api/src/040_axios_get_request/start/Example.jsx
+++ 16_rest_api/src/040_axios_get_request/start/Example.jsx
@@ -1,14 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Example = () => {
+
+  const [users, setUsers] = useState([])
+
   useEffect(() => {
     const getUser = async () => {
       const res = await axios.get(" http://127.0.0.1:3003/user")
-        console.log(res.data)
-      }
-      getUser();
-    })
-  };
+      setUsers(res.data)
+    }
+    getUser();
+  },[])
 
-  export default Example;
+  return (
+    <div>
+      {users.map(user => {
+
+        return (
+          <div key={user.id}>
+            <h3>{user.username}</h3>
+            <p>age: {user.age}</p>
+            <p>hobby: {user.hobbies.join(",")}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+};
+
+export default Example;
